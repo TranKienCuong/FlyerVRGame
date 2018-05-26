6b93c21eb704c71e796ca0d69e1b6614579ffae7
Kien Cuong Tran <trankiencuong10@gmail.com>
2018-05-26 14:10:49 +0700

    initial commit

---
+++ Assets/VRSampleScenes/Scripts/Flyer/OpponentLaser.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Flyer
+{
+    public class OpponentLaser : MonoBehaviour
+    {
+        [SerializeField] private float m_Speed = 500f;              // The speed each laser moves forward at.
+        [SerializeField] private float m_LaserLifeDuration = 3f;    // How long the laser lasts before it's returned to it's object pool.
+
+
+        private Rigidbody m_RigidBody;                              // Reference to the rigidbody of the laser.
+        private bool m_Hit;                                         // Whether the laser has hit something.
+        private FlyerHealthController flyerHealthController;
+        private GameObject m_Flyer;
+
+        public ObjectPool ObjectPool { private get; set; }          // The object pool the laser belongs to.
+
+
+        private void Awake()
+        {
+            flyerHealthController = FindObjectOfType<FlyerHealthController>();
+            m_Flyer = flyerHealthController.gameObject;
+            m_RigidBody = GetComponent<Rigidbody>();
+        }
+
+
+        private void Update()
+        {
+            m_RigidBody.MovePosition(m_RigidBody.position - transform.forward * m_Speed * Time.deltaTime);
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Opponent hit...");
+
+            // Only continute if the opponent laze hit the flyer
+            if (other.gameObject != m_Flyer)
+                return;
+
+            // Damage the flyer
+
+            m_Hit = true;
+            
+            Debug.Log("Flyer take damage");
+            flyerHealthController.TakeDamage(10);
+            
+
+            // Return the laser to the object pool.
+            ObjectPool.ReturnGameObjectToPool(gameObject);
+            Debug.Log("opponent laser hit...");
+            
+        }
+
+
+        private IEnumerator Timeout()
+        {
+            // Wait for the life time of the laser.
+            yield return new WaitForSeconds(m_LaserLifeDuration);
+
+            // If the laser hasn't hit something return it to the object pool.
+            if (!m_Hit)
+                ObjectPool.ReturnGameObjectToPool(gameObject);
+        }
+
+
+        public void Restart()
+        {
+            // At restart the laser hasn't hit anything.
+            m_Hit = false;
+
+            // Start the lifetime timeout.
+            StartCoroutine(Timeout());
+        }
+        
+    }
+}
---
+++ Assets/VRSampleScenes/Scripts/Flyer/OutsideObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using VRStandardAssets.Common;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace VRStandardAssets.Flyer
+{
+    class OutsideObject : MonoBehaviour
+    {
+        public event Action<OutsideObject> OnOutsideObjectRemovalDistance;    // This event is triggered when it is far enough behind the camera to be removed.
+
+        [SerializeField] private float m_OutsideObjectMinSize = 2f;     // The minimum amount an OutsideObject can be scaled up.
+        [SerializeField] private float m_OutsideObjectMaxSize = 3f;     // The maximum amount an OutsideObject can be scaled up.
+
+        private Transform m_Cam;                                    // Reference to the camera so this can be destroyed when it's behind the camera.
+
+        private const float k_RemovalDistance = 50f;                // How far behind the camera the OutsideObject must be before it is removed.
+
+
+        private void Awake()
+        {
+            m_Cam = Camera.main.transform;
+        }
+
+
+        private void Start()
+        {
+            // Set a random scale for the OutsideObject.
+            float scaleMultiplier1 = Random.Range(m_OutsideObjectMinSize, m_OutsideObjectMaxSize);
+            float scaleMultiplier2 = 2 * Random.Range(m_OutsideObjectMinSize, m_OutsideObjectMaxSize);
+            float scaleMultiplier3 = Random.Range(m_OutsideObjectMinSize, m_OutsideObjectMaxSize);
+            transform.localScale = new Vector3(scaleMultiplier1, scaleMultiplier2, scaleMultiplier3);
+        }
+
+
+        private void Update()
+        {
+            // If the OutsideObject is far enough behind the camera and something has subscribed to OnOutsideObjectRemovalDistance call it.
+            if (transform.position.z < m_Cam.position.z - k_RemovalDistance)
+                if (OnOutsideObjectRemovalDistance != null)
+                    OnOutsideObjectRemovalDistance(this);
+        }
+
+
+        private void OnDestroy()
+        {
+            // Ensure the events are completely unsubscribed when the OutsideObject is destroyed.
+            OnOutsideObjectRemovalDistance = null;
+        }
+    }
+}
